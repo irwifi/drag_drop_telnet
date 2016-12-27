3fe235c7ec50fcce6a330305529e6039742eeb8c
Rnwebdk <Cashamir@gmail.com>
2016-12-27 02:38:50 +0100

    Swipe and click navigation
     - Fixed the swipe navigation animation with slick + markup and icons

--- js/main.js
+++ js/main.js
@@ -1,23 +1,68 @@
 $(document).ready(function(){
 
-	$("#arrow").click(function() {
-	  	$('.price-menu').toggle('slide', {direction: 'down'});
-	  	var src = $('#arrow').attr('src');
+	// CURRENT SOLUTION
+	$("#arrow1").click(function() {
+	  	$('.currentprice').toggle('slide', {direction: 'down'});
+	  	var src = $('#arrow1').attr('src');
 	  	
 
 	  	// if the arrow of the event is pointing up change it to down and opposite
-	  	 var src = ($("#arrow").attr('src') === 'img/uparrow.jpg')
+	  	 var src = ($("#arrow1").attr('src') === 'img/uparrow.jpg')
             ? 'img/downarrow.jpg'
             : 'img/uparrow.jpg';
-         $("#arrow").attr('src', src);
-    	
+         $("#arrow1").attr('src', src);
+	});
 
+
+	// SOLUTION 1
+	$("#arrow2").click(function() {
+	  	$('.price-solution1').toggle('slide', {direction: 'down'});
+	  	var src = $('#arrow2').attr('src');
+
+	  	// if the arrow of the event is pointing up change it to down and opposite
+	  	 var src = ($("#arrow2").attr('src') === 'img/uparrow.jpg')
+            ? 'img/downarrow.jpg'
+            : 'img/uparrow.jpg';
+         $("#arrow2").attr('src', src);
+	});
+
+
+	// SOLUTION 1
+	$("#arrow3").click(function() {
+	  	$('.price-solution2').toggle('slide', {direction: 'down'});
+	  	var src = $('#arrow3').attr('src');
+	  	
+
+	  	// if the arrow of the event is pointing up change it to down and opposite
+	  	 var src = ($("#arrow3").attr('src') === 'img/uparrow.jpg')
+            ? 'img/downarrow.jpg'
+            : 'img/uparrow.jpg';
+         $("#arrow3").attr('src', src);
+	});
+
+
+	// Settingsbox
+	$("#settingsBtn").click(function() {
+	  	$('.SettingsBox').toggle('slide', {direction: 'down'});
 	});
 
 	// $(".servicesBtn").click(function() {
 	//   	$('.services-menu').toggle('slide', {direction: 'down'});
 
 	// });
+
+	$(".slider").slick({
+        // dots: true,
+        infinite: false,
+        // centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        vertical: true,
+        verticalSwiping: true,
+        arrows: true,
+        prevArrow: $('#navup'),
+		nextArrow: $('#navdown')
+      });
 
 }); // ready
 
